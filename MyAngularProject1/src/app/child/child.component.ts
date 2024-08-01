02e7682039a5791fb4bd66bb3f90641c05e2fff3
Priyadharshini Narayanan <priyanarayanan2603@gmail.com>
2024-08-01 09:54:06 +0530

    angularfiles

--- MyAngularProject1/src/app/child/child.component.ts
+++ MyAngularProject1/src/app/child/child.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalculatorService } from '../calculator.service';
 
 @Component({
   selector: 'app-child',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
-
+Sum : number;
+Sub : number;
+constructor(private calc :CalculatorService) {
+  this.Sum=calc.getAddition(10,29);
+  this.Sub=calc.getSubtraction(10,29);
+}
 }
